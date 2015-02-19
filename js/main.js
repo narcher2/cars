6fbff96bf9554cb3bfb487fe908c6fb06321d95a
narcher2 <narcher2@gmu.edu>
2015-02-19 08:41:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ function createGuards () {
     }*/
             var guard = guards.create(32, 64, 'guy');
             guard.anchor.setTo(0.5, 0.5);
-            guard.animations.add('fly', [ 0, 1, 2, 1 ], 20, true);
+            guard.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
             guard.play('fly');
             guard.body.moves = false;
     
@@ -94,7 +94,7 @@ function createGuards () {
     guards.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(guards).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    //var tween = game.add.tween(guards).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
 }
 
