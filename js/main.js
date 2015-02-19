5ef5f54ca7d991a651e3441d843b21fc5b4b912b
narcher2 <narcher2@gmu.edu>
2015-02-19 13:03:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ function createGuards () {
             //guard.body.moves = false;
             guard.body.bounce.y = 0.2;
             guard.body.collideWorldBounds = true;
-            guard.body.setSize(20, 32, 5, 16);
+            //guard.body.setSize(20, 32, 5, 16);
     
 
     guards.x = 100;
